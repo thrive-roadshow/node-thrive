3cbdf83729278d3f95352d0a01f3d75c9dc44383
Farid Tri Wicaksono <farid.wicak@gmail.com>
2024-05-10 11:26:16 +0700

    fix: adjustment regex password

--- src/modules/user/repositories/commands/command_model.js
+++ src/modules/user/repositories/commands/command_model.js
@@ -18,7 +18,10 @@ const register = joi.object({
     joi.string().email(),
     joi.string().regex(/^[+]62/)
   ],
-  password: joi.string().min(8).regex(/(?=\D*\d)(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*(),.?":{}|<>]{6,}$/).required(),
+  password: joi.string().min(8).pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/).required().messages({
+    'string.pattern.base': 'Password harus memiliki setidaknya 8 karakter, dengan minimal satu huruf besar, satu huruf kecil, satu angka, dan satu karakter khusus.',
+    'any.required': 'Password diperlukan.'
+  }),
   referralCode: joi.string().optional().allow('')
 });
 
